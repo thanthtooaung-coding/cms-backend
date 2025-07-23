1d4223415a3269bc1914efaeebad3db4caf1ddad
thanthtooaung <thanthtoo1285@gmail.com>
2025-07-24 02:27:48 +0700

    feat: setup LMS Backend with Maven

---
+++ app/lms-sys/src/test/java/com/content_management_system/lms/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.content_management_system.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
