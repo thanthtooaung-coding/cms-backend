54c2c7f6e89bae8c951509145b4804af825760ee
thanthtooaung <thanthtoo1285@gmail.com>
2025-07-25 22:42:39 +0700

    feat: added Swagger in LMS System

---
+++ app/lms-sys/src/main/java/com/content_management_system/lms/shared/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.content_management_system.lms.shared.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("lms-public")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI lmsOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("LMS API")
+                        .description("Learning Management System API documentation")
+                        .version("v1.0.0"));
+    }
+}
